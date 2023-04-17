d62baf8d4ff0b21948388313635b3765191f60b9
Jonatha PAIN-CHAMMING'S <jonathan.pchs@gmail.com>
2023-04-17 11:03:08 +0200

    Intégration de PHPmailer

--- controller/utilisateurs/pw_reset.php
+++ controller/utilisateurs/pw_reset.php
@@ -1,7 +1,9 @@
 Controller utilisateur PW_RESET | 
 
+<?php
+
 // On supprime le mot de passe en BDD
 
 // On créé un token
-
+if(isset($_GET['mail']))
 // On envoie un email à l'utilisateur pour lui transmettre le token
