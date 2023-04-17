72abe4918f05f3d543b159cce23cb0af4f265298
Jonatha PAIN-CHAMMING'S <jonathan.pchs@gmail.com>
2023-04-17 13:32:05 +0200

    Modification du nom de la variable de configuration

--- mail.php
+++ mail.php
@@ -16,15 +16,15 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = $js['smtp']['host'];                    //Set the SMTP server to send through
+    $mail->Host       = $config['smtp']['host'];                    //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = $js['smtp']['username'];               //SMTP username
-    $mail->Password   = $js['smtp']['password'];                //SMTP password
+    $mail->Username   = $config['smtp']['username'];               //SMTP username
+    $mail->Password   = $config['smtp']['password'];                //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = $js['smtp']['port'];                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = $config['smtp']['port'];                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom($js['smtp']['username']);
+    $mail->setFrom($config['smtp']['username']);
     $mail->addAddress('jonathan.pchs@gmail.com');     //Add a recipient
     // $mail->addAddress('ellen@example.com');               //Name is optional
     // $mail->addReplyTo('info@example.com', 'Information');
